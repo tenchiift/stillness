e773368d837ad21a54626f29194fc8213849090c
tenchiift <akmalizzat160@gmail.com>
2026-04-11 15:29:16 +0800

    Initial Commit

---
+++ stillness/stillnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  stillnessApp.swift
+//  stillness
+//
+//  Created by tenchii on 11/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct stillnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
